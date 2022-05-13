046e85e3d17edc9b26cb524321be13bc2ad4fb45
LeviUniAcc <otterbach.levi@gmail.com>
2022-05-13 23:00:55 +0200

    init

---
+++ src/main/java/de/unistuttgart/hackathon/taser/QueueService/QueueServiceApplication.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.hackathon.taser.QueueService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueServiceApplication.class, args);
+	}
+
+}
